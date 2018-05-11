dce3d289f3ea59edd929b44f21ffe58ea91a924f
craycrayfish <shawntan72@gmail.com>
2018-05-11 20:10:33 +0100

    Add files via upload

--- simulator3x3.py
+++ simulator3x3.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 11 18:58:46 2018
+
+@author: shawn
+"""
+
 import numpy as np
+import scipy as sp
+import math
+import pandas as pd
