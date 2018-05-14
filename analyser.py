3210df1c1a9ae6ceafa0df1ba70df496bda2aa3f
shawn <shawntan72@gmail.com>
2018-05-14 21:54:10 +0100

    Analysis program created

---
+++ analyser.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 14 21:42:15 2018
+
+@author: shawn
+"""
+
+import scipy as sp
+import matplotlib.pyplot as plt
+import math
+from simulatornxn import simulate
+
+
+'''
+
+Investigating the variation in edge of starting first 
+
+
+'''
+simulate(100,5,4)
